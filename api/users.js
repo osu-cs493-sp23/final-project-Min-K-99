61f71d03a29e416fde0d7be5ee5f97376bf49b32
NicksonEdbert <nicksonedbert@gmail.com>
2023-06-12 23:16:40 -0700

    renaming files names

--- api/users.js
+++ api/users.js
@@ -1,54 +1,56 @@
-const { Router } = require('express')
+const { Router } = require("express");
 
-const { Business } = require('../models/business')
-const { Photo } = require('../models/photo')
-const { Review } = require('../models/review')
+const { Business } = require("../models/assignment");
+const { Photo } = require("../models/course");
+const { Review } = require("../models/user");
 
-const router = Router()
+const router = Router();
 
 /*
  * Route to list all of a user's businesses.
  */
-router.get('/:userId/businesses', async function (req, res) {
-  const userId = req.params.userId
+router.post("/", async function (req, res) {
+  const userId = req.params.userId;
   try {
-    const userBusinesses = await Business.findAll({ where: { ownerId: userId }})
+    const userBusinesses = await Business.findAll({
+      where: { ownerId: userId },
+    });
     res.status(200).json({
-      businesses: userBusinesses
-    })
+      businesses: userBusinesses,
+    });
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
 
 /*
  * Route to list all of a user's reviews.
  */
-router.get('/:userId/reviews', async function (req, res) {
-  const userId = req.params.userId
+router.post("/login", async function (req, res) {
+  const userId = req.params.userId;
   try {
-    const userReviews = await Review.findAll({ where: { userId: userId }})
+    const userReviews = await Review.findAll({ where: { userId: userId } });
     res.status(200).json({
-      reviews: userReviews
-    })
+      reviews: userReviews,
+    });
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
 
 /*
  * Route to list all of a user's photos.
  */
-router.get('/:userId/photos', async function (req, res) {
-  const userId = req.params.userId
+router.get("/:userId", async function (req, res) {
+  const userId = req.params.userId;
   try {
-    const userPhotos = await Photo.findAll({ where: { userId: userId }})
+    const userPhotos = await Photo.findAll({ where: { userId: userId } });
     res.status(200).json({
-      photos: userPhotos
-    })
+      photos: userPhotos,
+    });
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
