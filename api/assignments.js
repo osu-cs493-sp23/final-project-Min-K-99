67c86e839534b0d94596ea2c2674b6a34983d396
Tun Aung Thaung <alphatrole@gmail.com>
2023-06-14 01:47:26 -0700

    PARTIALLY DONE IN /api/assignments.js and /model/assignment.js BUT STILL NEED A LOT OF WORK

--- api/assignments.js
+++ api/assignments.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { connectToDb } = require("../lib/mongo");
+const { getDbReference } = require("../lib/mongo");
 const { validateAgainstSchema } = require("../lib/validation");
 
-const { AssignmentSchema, insertNewAssignment, getAssignmentById } = require("../models/assignment");
+const { AssignmentSchema, SubmissionSchema, insertNewAssignment, insertNewSubmission, getAssignmentById, getSubmissionById, deleteAssignmentById } = require("../models/assignment");
 const { generateAuthToken, requireAuthentication } = require("../lib/auth");
+const { ObjectId } = require("bson");
 const router = Router();
 
 /*
@@ -14,7 +15,7 @@ router.post("/", requireAuthentication ,async function (req, res, next) {
         try{
             const id = await insertNewAssignment(req.body)
             res.status(201).send({
-                _id: id
+                id: id
             })
         } catch (e) {
             next(e)
@@ -50,25 +51,68 @@ router.get("/:assignmentId", async function (req, res, next) {
 /*
  * Route to update an assignment based on assignmentId
  */
-router.patch("/:assignmentId", async function (req, res, next) {
+router.patch("/:assignmentId", requireAuthentication, async function (req, res, next) {
 });
 
 /*
  * Route to delete an assignment based on assignmentId
  */
-router.delete("/:assignmentId", async function (req, res, next) {
+router.delete("/:assignmentId", requireAuthentication, async function (req, res, next) {
+    try{
+        const assignment = await deleteAssignmentById(req.params.assignmentId)
+        if(assignment){
+            res.status(200).send({
+                msg: "SUCCEEDED"
+            })
+        } else {
+            next()
+        }
+    } catch (e) {
+        next(e)
+    }
 });
 
 /*
- * Route to delete an assignment.
+ * Route to get submission based on assignmentId
  */
-router.get("/:assignmentId/submissions", async function (req, res, next) {
+router.get("/:assignmentId/submissions", requireAuthentication, async function (req, res, next) {
+    // try{
+    //     const submission = await getAssignmentById(req.params.assignmentId)
+    //     if(assignment){
+    //         res.status(200).send({
+    //             courseId : assignment.courseId,
+    //             title : assignment.title,
+    //             poitns: assignment.points,
+    //             due: assignment.due
+    //         })
+    //     } else {
+    //         next()
+    //     }
+    // } catch (e) {
+    //     next (e)
+    // }
 });
 
 /*
- * Route to delete a photo.
+ * Route to post a submission for assignment
+ * This functions prob need to be a child of assignments and need to be created based on parent.
+ * Function is partially done but still need a lot of work
  */
-router.post("/:assignmentId/submissions", async function (req, res, next) {
+router.post("/:assignmentId/submissions", requireAuthentication, async function (req, res, next) {
+    if(validateAgainstSchema(req.body, SubmissionSchema)){
+        try{
+            const id = await insertNewSubmission(req.body)
+            res.status(201).send({
+                id: id
+            })
+        } catch (e) {
+            next(e)
+        }
+    } else {
+        res.status(400).send({
+            error: "Requested body does not contain a valid Assignment."
+        })
+    }
 });
 
 module.exports = router;
