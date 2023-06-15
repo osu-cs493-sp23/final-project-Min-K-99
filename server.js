98312a794bf596427b67404f637ac9478bf90185
AlexYu84 <yuale@oregonstate.edu>
2023-06-15 04:21:23 -0700

    implemented rate limiting (Partially)
    implemented rate limiting
    make sure you do 'npm install redis' as well as make a redis-server in docker: 'docker run -d --name redis-server -p 6379:6379 redis'

--- server.js
+++ server.js
@@ -8,12 +8,67 @@ require('dotenv').config()
 
 const express = require('express')
 const morgan = require('morgan')
+const redis = require('redis')
 
 const api = require('./api')
 const { connectToDb } = require("./lib/mongo");
 
 const app = express()
 const port = process.env.PORT || 8000
+
+const redisHost = process.env.REDIS_HOST || "localhost"
+const redisPort = process.env.REDIS_PORT || "6379"
+const redisClient = redis.createClient({
+  url: `redis://${redisHost}:${redisPort}`
+})
+
+const rateLimitWindowMillis = 60000
+const rateLimitMaxRequests = 10
+const correctRateLimit = 30
+const correctRateLimitRefreshRate = correctRateLimit / rateLimitWindowMillis  
+const rateLimitRefreshRate = rateLimitMaxRequests / rateLimitWindowMillis
+
+async function rateLimit(req, res, next){
+  // const isTokenValid = req.headers.authorization === 
+
+  let tokenBucket 
+  try{
+    tokenBucket = await redisClient.hGetAll(req.ip)
+  }catch (e){
+    next()
+    return
+  }
+
+  tokenBucket = {
+    tokens: parseFloat(tokenBucket.tokens) || rateLimitMaxRequests,
+    last: parseInt(tokenBucket.last) || Date.now()
+  }
+
+  const timestamp = Date.now()
+  const ellapsedMillis = timestamp - tokenBucket.last
+  tokenBucket.tokens += ellapsedMillis * rateLimitRefreshRate
+  tokenBucket.tokens = Math.min(tokenBucket.tokens, rateLimitMaxRequests)
+  tokenBucket.last = timestamp
+
+  if(tokenBucket.tokens >= 1){
+    tokenBucket.tokens -= 1
+    await redisClient.hSet(req.ip, [
+      ["tokens", tokenBucket.tokens],
+      ["last", tokenBucket.last]
+    ])
+    next()
+  } else{
+    await redisClient.hSet(req.ip, [
+      ["tokens", tokenBucket.tokens],
+      ["last", tokenBucket.last]
+    ])
+    res.status(429).send({
+      error: "Too many requests per minute"
+    })
+  }
+}
+
+app.use(rateLimit)
 
 /*
  * Morgan is a popular request logger.
@@ -51,7 +106,9 @@ app.use('*', function (err, req, res, next) {
  * to the MySQL server.
  */
 connectToDb(async function () {
-  app.listen(port, function () {
-    console.log("== Server is running on port", port);
-  });
+  redisClient.connect().then(function () {
+    app.listen(port, function () {
+      console.log("== Server is running on port", port);
+    });
+  })
 });
